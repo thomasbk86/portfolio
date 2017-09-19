0b42ce31bf9284af973fbab0629051f40dbbcb18
Thomas Kurth <thomasbk86@gmail.com>
2017-09-19 14:32:12 -0400

    images

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
-$(".cross").hide();
-$(".menu").hide();
-$(".hamburger").click(function() {
-$(".menu").slideToggle("slow", function() {
-$(".hamburger").hide();
-$(".cross").show();
-});
-});
+    $(".cross").hide();
+    $(".menu").hide();
+    $(".hamburger").click(function() {
+      $(".menu").slideToggle("slow", function() {
+      $(".hamburger").hide();
+      $(".cross").show();
+    });
+  });
 
-$(".cross").click(function() {
-$(".menu").slideToggle("slow", function() {
-$(".cross").hide();
-$(".hamburger").show();
-});
-});
+    $(".cross").click(function() {
+      $(".menu").slideToggle("slow", function() {
+      $(".cross").hide();
+      $(".hamburger").show();
+    });
+  });
 
 });
